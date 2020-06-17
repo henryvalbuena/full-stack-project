3f2632053e652cb49b7e31d14108392a1b0864e1
Henry Valbuena <dev.henryvm@gmail.com>
2020-06-17 11:29:24 -0400

    dockerized full-stack app

--- backend/src/api.py
+++ backend/src/api.py
@@ -10,7 +10,7 @@ from auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 """
 @TODO uncomment the following line to initialize the datbase
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://backend:8081/api', // the running FLASK api server url
+  apiServerUrl: 'http://localhost:8081/api', // the running FLASK api server url
   auth0: {
     url: 'coffe-shop-project', // the auth0 domain prefix
     audience: 'drinks', // the audience set for the auth0 app
